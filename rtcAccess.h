362f9c6650bcd9cf5d883af3d5f0b4f5ed4f4c15
Bruno <delattreb@gmail.com>
2017-08-13 12:20:55 +0200

    Ajoutez des fichiers projet.

---
+++ rtcAccess.h
@@ -0,0 +1,65 @@
+//
+// Created by Bruno on 30/06/2017.
+//
+
+#include "RTClib.h"
+#include "var.h"
+
+
+class rtcAccess {
+
+private:
+
+	DS1307 rtc;
+
+public:
+
+	bool timeset = false;
+
+	rtcAccess() {
+	}
+
+	~rtcAccess() {
+	}
+
+	void init() {
+		rtc.begin();
+		if (!rtc.isrunning()) {
+#ifdef DEBUG
+			Serial.println("RTC is NOT running");
+#endif 
+			rtc.adjust(DateTime(__DATE__, __TIME__));
+#ifdef DEBUG
+			Serial.println("Date and time adjusted");
+			Serial.println(getDateStr());
+			Serial.println(getTimeStr());
+#endif 
+		}
+		else {
+#ifdef DEBUG
+			Serial.println("RTC is running");
+			Serial.println(getDateStr());
+			Serial.println(getTimeStr());
+#endif 
+		}
+	}
+
+	DateTime getDateTime() {
+		DateTime now = rtc.now();
+		return now;
+	}
+
+	String getDateStr()
+	{
+		DateTime now = rtc.now();
+		return String(now.day()) + "/" + String(now.month()) + "/" + String(now.year());
+	}
+
+	String getTimeStr() {
+		DateTime now = rtc.now();
+		return String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second());
+	}
+};
+
+
+
