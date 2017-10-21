ee3a820676fc043dfcc21d0ada37e17edf5349ab
DELATTRE, BRUNO <bruno.delattre@econocom.com>
2017-10-21 18:22:34 +0200

    Update

--- Arduino stratoballoon.ino
+++ Arduino stratoballoon.ino
@@ -12,7 +12,6 @@
 #include "libLED.h"
 
 #ifdef ARDUINO_1
-#include "libBME280.h"
 #include "libGPS.h"
 #include "lib18b20.h"
 #include "libSI7021.h"
@@ -21,15 +20,22 @@
 
 #ifdef ARDUINO_2
 #include "libMPU6050.h"
+#include "libBME280.h"
 #endif
 
 #pragma region Global var
+long lat, lon;
+unsigned long age;
+int16_t ax, ay, az, gx, gy, gz;
+float gpsaltitude, dstemp, temp, hum, pres, sitemp, sihum, dewpoint, gpsalt, speed, gpscourse;
+String file;
+bool bstart = false, bstop = false;
+
 libSD sda;
 libDS1307 rtc;
 libLED led;
 
 #ifdef ARDUINO_1
-libBME280 bme;
 libGPS gps;
 lib18b20 ds18b20;
 libSI7021 si7021;
@@ -37,11 +43,10 @@ libSI7021 si7021;
 #endif 
 
 #ifdef ARDUINO_2
+libBME280 bme;
 libMPU6050 gyro;
 #endif 
 #pragma endregion 
-
-int cpt = 0;
 
 //
 // Setup
@@ -53,6 +58,10 @@ void setup() {
 		;
 	}
 #endif
+	//Button
+	pinMode(PIN_START, INPUT);
+	pinMode(PIN_STOP, INPUT);
+
 	//Init Pin for LED
 	pinMode(LED_PIN, OUTPUT);
 
@@ -60,22 +69,21 @@ void setup() {
 	SPI.begin();
 	Wire.begin();
 	Wire.setClock(400000); // 400kHz I2C clock. 
+	rtc.init();
+	sda.init();
+	led.BlinkLed(LED_BLINK_INIT, LED_BLINK_TIME);
 
 #ifdef ARDUINO_1
 	ds18b20.begin();
 	gps.begin();
-	bme.init();
 	si7021.init();
-	rtc.init();
 	//aprs.init();
 #endif
 
 #ifdef ARDUINO_2
+	bme.init();
 	gyro.begin();
 #endif
-
-	sda.init();
-	led.BlinkLed(LED_BLINK_INIT, LED_BLINK_INIT_TIME);
 }
 
 //
@@ -83,55 +91,58 @@ void setup() {
 //
 void loop()
 {
-	long lat, lon;
-	unsigned long age;
-	int16_t ax, ay, az, gx, gy, gz;
-	float gpsaltitude, dstemp, temp, hum, pres, sitemp, sihum, dewpoint, gpsalt, speed, gpscourse;
-	String file;
-
-
-	//TODO Button Start & stop
-
-#ifdef ARDUINO_1
-	//aprs.locationUpdateExample();
-	//aprs.test();
-
-	// ACQ GPS
-	if (gps.getData()) {
-		gps.getPosition(&lat, &lon, &age);
-		gps.getAltitude(&gpsalt);
-		gps.getCourse(&gpscourse);
-		gps.getSpeed(&speed);
+	// Read button
+	if (digitalRead(PIN_START) == HIGH) {
+		bstart = true;
+		bstop = false;
+		led.BlinkLed(LED_BLINK_START, LED_BLINK_TIME);
+	}
+	if (digitalRead(PIN_STOP) == HIGH) {
+		bstop = true;
+		bstart = false;
+		led.BlinkLed(LED_BLINK_STOP, LED_BLINK_TIME);
 	}
 
-	// ACQ BME208
-	bme.getData(&temp, &hum, &pres);
+	if (bstart && !bstop) {
+#ifdef ARDUINO_1
+		//aprs.locationUpdateExample();
+		//aprs.test();
 
-	// ACQ DS18B20
-	ds18b20.getData(&dstemp);
+		// ACQ GPS
+		if (gps.getData()) {
+			gps.getPosition(&lat, &lon, &age);
+			gps.getAltitude(&gpsalt);
+			gps.getCourse(&gpscourse);
+			gps.getSpeed(&speed);
+		}
 
-	// Acq SI7021
-	sitemp = si7021.getTemperature();
-	sihum = si7021.getHumidity();
+		// ACQ DS18B20
+		ds18b20.getData(&dstemp);
+
+		// Acq SI7021
+		sitemp = si7021.getTemperature();
+		sihum = si7021.getHumidity();
 #endif 
 
 #ifdef ARDUINO_2
-	// ACQ Gyro
-	gyro.getData(&ax, &ay, &az, &gx, &gy, &gz);
+		// ACQ BME280
+		bme.getData(&temp, &hum, &pres);
+
+		// ACQ Gyro
+		gyro.getData(&ax, &ay, &az, &gx, &gy, &gz);
 #endif 
 
-	DateTime now = rtc.getDateTime();
-	// Save data
-	file = LOG_FILE_DATA + String(now.hour()) + ".csv";
-	if (cpt < 1000)
+		DateTime now = rtc.getDateTime();
+		// Save data
+		file = LOG_FILE_DATA + String(now.hour()) + ".csv";
 		sda.WriteData(lat, lon, gpsalt, gpscourse, speed, ax, ay, az, gx, gy, gz, dstemp, temp, hum, pres, rtc.getDateTimeStrEn(), file);
 
 #ifdef ARDUINO_1
-	delay(ACQ_FREQUENCY_1);
+		delay(ACQ_FREQUENCY_1);
 #endif
 
 #ifdef ARDUINO_2
-	delay(ACQ_FREQUENCY_2);
+		delay(ACQ_FREQUENCY_2);
 #endif
-	cpt += 1;
+	}
 }
--- var.h
+++ var.h
@@ -4,7 +4,7 @@
 
 //Software Version
 #define APPNAME "Stratoballoon"
-#define APPVERSION "2.1"
+#define APPVERSION "2.3"
 
 // Arduino
 //#define ARDUINO_1
@@ -13,30 +13,41 @@
 #define DEBUG
 #define INFO
 
+// BUTTON
+#define PIN_START 2
+#define PIN_STOP 3
+
 //SoftwareSerial
 #define RXPIN 6
 #define TXPIN 5
+
+//SoftwareSerial -> Arduino 3
+#define RXPIN 7
+#define TXPIN 8
+
+//Arduino N°3
+//RX1 Pin 2 RX2 Pin 3
+//TX1 4 TX2 Pin 5
 
 //Acquisition frequency
 #define ACQ_FREQUENCY_1 10000
 #define ACQ_FREQUENCY_2 2000
 #define ACQ_GPS_DURATION 800
 
-//MPU6050
-#define INTERRUPT_PIN 2  
-
 //DS18B20
-#define ONE_WIRE_BUS 7
+#define ONE_WIRE_BUS 4
 
 //LED
-#define LED_PIN 8
+#define LED_PIN 9
 #define LED_BLINK_TIME 50
 #define LED_BLINK_INIT 10
-#define LED_BLINK_INIT_TIME 100
+#define LED_BLINK_START 5
+#define LED_BLINK_STOP 2
+#define LED_BLINK_TIME 100
 
 
 //SD Access Pin
-#define SD_PIN 10
+#define SD_CS_PIN SS //Pin N°10
 #define LOG_FILE_DATA "log"
 
 //Serial
@@ -47,5 +58,3 @@
 #define OK 0
 #define NOK -1
 #define READY 1
-
-
